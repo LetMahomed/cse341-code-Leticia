7da1248ce86f9dab9b8d87543336f24ba80338ee
LetMahomed <ldesousa@byupathway.edu>
2025-12-04 23:56:36 -0300

    lets see

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -10,7 +10,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:3000",
+        url: "https://cse341-code-leticia.onrender.com",
       },
     ],
     components: {
